3c3810e65e7e9d1957c065933fd3b84bdcf101e3
SViN24 <svin@dismail.de>
2022-10-24 08:59:45 +0300

    added checks for the length these include checks for if the digits even exists of if its an int

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ import sys
 arguments = sys.argv[1:]
 
 length = 8
-if "-l" in arguments:
-    index_pos = arguments.index("-l")
-    #TODO: need to think of a way to make this work better ,it does not reuse characters in the password(random.sample counts)
-    #TODO: check for int
-    length = int(arguments[index_pos + 1])
-    
 
 #initialize combination string
 combination = ""
@@ -25,6 +19,28 @@ if "--no-digit" not in arguments:
     combination += string.digits
 if "--no-punk" not in arguments:
     combination += string.punctuation
+
+
+#TODO: need to think of a way to make this work better ,it does not reuse characters in the password(random.sample counts)
+
+if "-l" in arguments:
+    index_pos = arguments.index("-l")
+    
+    #Check if length value exists (-1 is for adjustment's sake)
+    if index_pos + 1 > len(arguments) - 1:
+        sys.exit("No number included")
+    #Check if value is interger
+    if not arguments[index_pos + 1].isdigit():
+        sys.exit("Length needs to be a number")
+    #Cast to to interget and store
+    length = int(arguments[index_pos + 1])
+    
+    # interger checks
+    if len(combination) < length:
+        sys.exit("Length too large")
+    if length <= 0:
+        sys.exit("Length value invalid")
+    
 
 # error handle
 if not combination:
